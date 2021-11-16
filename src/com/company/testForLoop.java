d0a9039299ada2c060e0d45ce7f4e8abbe4a09a5
olamilec <olamilec@gmail.com>
2021-11-16 15:25:42 +0800

    Java Tutorials

---
+++ src/com/company/testForLoop.java
@@ -0,0 +1,32 @@
+package com.company;
+
+public class testForLoop {
+    public static void main(String[] args){
+        tForLoop();
+    }
+    public static void tForLoop(){
+        int [] myArrayNew;
+        myArrayNew = new int[9];
+        /*myArrayNew[0] = 2;
+        myArrayNew[1] = 4;
+        myArrayNew[2] = 6;
+        myArrayNew[3] = 8;
+        myArrayNew[4] = 10;
+        myArrayNew[5] = 12;
+        myArrayNew[6] = 14;
+        myArrayNew[7] = 18;
+        myArrayNew[8] = 20;
+        System.out.println(myArrayNew[0]);
+        System.out.println(myArrayNew[1]);
+        System.out.println(myArrayNew[2]);
+        System.out.println(myArrayNew[3]);
+        System.out.println(myArrayNew[4]);
+        System.out.println(myArrayNew[5]);
+        System.out.println(myArrayNew[6]);*/
+
+        for (int i = 0; i < myArrayNew.length; i++){
+            System.out.println(i);
+        }
+
+    }
+}
