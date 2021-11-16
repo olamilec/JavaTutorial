6e37c14c16c0df8ac2269a95381ab63595d62fe2
olamilec <olamilec@gmail.com>
2021-11-16 16:01:10 +0800

    Java Tutorials

---
+++ src/com/company/whileLoop.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class whileLoop {
+    public static void main(String[] args){
+        int number = 0;
+        while(number < 5){
+            System.out.println("number + 1 everytime number not up to 5 ");
+            System.out.println(number);
+            number += 1;
+        }
+    }
+}
