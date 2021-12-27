fe6c8db648f22d976f8f0e1d916c5ec277d9a1fe
olamilec <olamilec@gmail.com>
2021-12-28 01:28:09 +0800

    Java Tutorials

---
+++ src/com/company/loan.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class loan {
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("What is your loan amount: ");
+            int amount = scanner.nextInt();
+            int loan;
+            //your code goes here
+            for(int i=1;i<=3;i++)
+            {
+                loan =amount/10;
+                amount-=loan;
+            }
+            System.out.println(amount);
+        }
+    }
