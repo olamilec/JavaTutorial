0853ddc6de8b903545c45f3b6866bfc31c0da672
olamilec <olamilec@gmail.com>
2022-02-27 12:01:01 +0800

    Java Tutorials

---
+++ src/com/company/pattern.java
@@ -0,0 +1,36 @@
+package com.company;
+
+public class pattern {
+    public static void main(String[] args) {
+        pattern1(4);
+        System.out.println("-----------------");
+        pattern2(4);
+        System.out.println("-----------------");
+        pattern3(4);
+    }
+
+    public static void pattern1(int n) {
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= row; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    public static void pattern2(int n) {
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= n; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+    public static void pattern3(int n) {
+        for (int row = 1; row <= n; row++) {
+            for (int col = 1; col <= n; col++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
